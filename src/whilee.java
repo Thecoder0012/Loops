1f0d487466478bfe76f7e281339d706c26fecfad
Thecoder0012 <mardineliassi57@gmail.com>
2021-10-11 01:21:54 +0200

    Initial commit

---
+++ src/whilee.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class whilee {
+
+
+    public static void main(String[] args) {
+        hello();
+        Scanner input = new Scanner(System.in);
+        Random rand = new Random();
+
+
+
+
+    }
+
+    public static void hello(){
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("i = " + i);
+
+
+        }
+
+    }
+}
